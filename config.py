2d6e848c3c2ec23dbf92b4d86341c53903e055b3
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-05-23 07:45:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "24935727"))
-API_HASH = getenv("API_HASH", "3fd33336629324ecd664e9b6894f0909")
+API_ID = int(getenv("API_ID", "26729193"))
+API_HASH = getenv("API_HASH", "a94598ef642481e35466292df95f251e")
 BOT_TOKEN = getenv("BOT_TOKEN", "7609700133:AAG6TqHDZt3Mdi3Za5XKaP-xfcu0HFjwxTA")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7336971189").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
-LOG_GROUP = getenv("LOG_GROUP", "-1002262545667")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002483720229"))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1012164907").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Uploaderbot:Uploaderbot@cluster0.mkxbrre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002607015025")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002607015025"))
